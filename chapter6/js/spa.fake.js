529bae68543ceaa85e566559619a967ccb17e775
又是十三 <kaede008@qq.com>
2016-01-06 03:28:18 +0800

    add spa.model.chat update_avatar.

--- chapter6/js/spa.fake.js
+++ chapter6/js/spa.fake.js
@@ -40,7 +40,7 @@ spa.fake = (function(){
         };
 
         emit_sio = function(msg_type, data){
-            var person_map;
+            var person_map, i;
             // respond to 'adduser' event with 'userupdate'
             // callback after a 3s delay
             if (msg_type === 'adduser' && callback_map.userupdate){
@@ -79,6 +79,20 @@ spa.fake = (function(){
                     listchange_idto = undefined;
                 }
                 send_listchange();
+            }
+
+            // Create a handler for receipt of an updateavatar message.
+            // simulate send of 'updateavatar' message and data to server
+            if (msg_type === 'updateavatar' && callback_map.listchange){
+                // simulate receipt of 'listchange' message
+                for (i=0;i<peopleList.length;i++){
+                    if (peopleList[i]._id === data.person_id){
+                        peopleList[i].css_map = data.css_map;
+                        break
+                    }
+                }
+                // execute callback for the 'listchange' message
+                callback_map.listchange([peopleList]);
             }
         };
 
